d3aae4943bb32f325238d9b88f0cfaf22f623c32
AbuShahma022 <abushahma1014@gmail.com>
2026-07-09 23:41:31 +0600

    try fixing v1

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -2,4 +2,4 @@ import Stripe from "stripe";
 import config from "./index";
 
 
-export const stripe = new Stripe(config.stripe.secretKey);
+export const stripe : Stripe = new Stripe(config.stripe.secretKey);
